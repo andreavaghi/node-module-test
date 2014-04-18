53b70bd29841d8325b3035a7e106c84230d2a0a0
Andrea Vaghi <andrea.vaghi@gmail.com>
2014-04-18 08:52:03 +0200

    add first example of basic module

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('it works');
+var _ = require('underscore');
+
+var test = {
+    between: function(num, a, b) {
+        if (a === b) return "max and min are the same";
+        return (a < b) ? (num > a && num < b) : (num < a && num > b);
+    }
+}
+
+module.exports = test;
